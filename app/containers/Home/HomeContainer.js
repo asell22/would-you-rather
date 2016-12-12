5887c46dfd12b2e5f46bb30bc69ead714a2acbc1
asell22 <alexanderjsell@gmail.com>
2016-12-12 14:44:47 -0800

    Add styles to subheader in HomeContainer

--- app/containers/Home/HomeContainer.js
+++ app/containers/Home/HomeContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { header } from './styles.css'
+import { header, subheader } from './styles.css'
 const HomeContainer = React.createClass({
   render () {
     return (
-      <h1 className={ header }>{'Would You Rather'}</h1>
+      <div>
+        <h1 className={ header }>{'Would You Rather'}</h1>
+        <h3 className={ subheader }>{'The 100 year old American Classic'}</h3>
+      </div>
+
     )
   }
 })
