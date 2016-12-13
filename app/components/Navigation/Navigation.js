62a6f01d1f19f30771869a509368e610a3181088
asell22 <alexanderjsell@gmail.com>
2016-12-13 12:28:11 -0800

    Add proptypes to Navigation Component

--- app/components/Navigation/Navigation.js
+++ app/components/Navigation/Navigation.js
@@ -2,6 +2,10 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import { container, navContainer, link } from './styles.css'
 
+Navigation.propTypes = ActionLinks.propTypes = NavLinks.propTypes = {
+  isAuthed: PropTypes.bool.isRequired
+}
+
 const NavLinks = ({isAuthed}) => {
   return isAuthed === true
     ? <ul>
