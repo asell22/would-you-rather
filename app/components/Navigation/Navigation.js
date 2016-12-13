a869134961a36f1b59040fe3875ef0a23f3a6105
asell22 <alexanderjsell@gmail.com>
2016-12-13 12:23:03 -0800

    Add Navigation component

---
+++ app/components/Navigation/Navigation.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+import { container, navContainer, link } from './styles.css'
+
+const NavLinks = ({isAuthed}) => {
+  return isAuthed === true
+    ? <ul>
+        <li><Link className={link} to='/' className={link}>{'Home'}</Link></li>
+      </ul>
+    : null
+}
+
+
+const ActionLinks = ({isAuthed}) => {
+  return isAuthed === true
+  ? <ul>
+      <li>MODAL</li>
+      <li><Link className={link} to='/logout' className={link}>{'Logout'}</Link></li>
+    </ul>
+  : <ul>
+      <li><Link className={link} to='/' className={link}>{'Home'}</Link></li>
+      <li><Link className={link} to='/auth' className={link}>{'Authenticate'}</Link></li>
+    </ul>
+}
+
+const Navigation = ({isAuthed}) => (
+  <div className={container}>
+    <nav className={navContainer}>
+      <NavLinks isAuthed={isAuthed}/>
+      <ActionLinks isAuthed={isAuthed}/>
+    </nav>
+  </div>
+
+)
+
+export default Navigation
--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { container } from './styles.css'
+import { Navigation } from 'components'
+
 const MainContainer = React.createClass({
   render () {
     return (
       <div className={container}>
+        <Navigation isAuthed={true}/>
         {this.props.children}
       </div>
     )
